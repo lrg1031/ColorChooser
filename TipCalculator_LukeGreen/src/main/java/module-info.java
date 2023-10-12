320ec078fb2d38cd95af56b329c32e3e37b274cd
lrg1031 <90281038+lrg1031@users.noreply.github.com>
2023-10-11 21:58:09 -0400

    Delete TipCalculator_LukeGreen directory

--- TipCalculator_LukeGreen/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.tipcalculator_lukegreen {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.tipcalculator_lukegreen to javafx.fxml;
-    exports com.mycompany.tipcalculator_lukegreen;
-}
